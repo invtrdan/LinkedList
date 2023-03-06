c9f6018e4177c6878af0571939c1170d9c5b85a5
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-03-06 16:59:15 -0500

    created the Node class

---
+++ LinkedList.py
@@ -0,0 +1,7 @@
+class Node:
+    '''
+    Represents an individual element in the Linked List.
+    '''
+    def __init__(self, data=None, next=None):
+        self.data = data 
+        self.next = next 

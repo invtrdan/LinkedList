bdc6ca62b2c3b97a85014d222a578638dd45a794
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-03-06 18:00:46 -0500

    insert_at_end function created and tested

--- LinkedList.py
+++ LinkedList.py
@@ -1,6 +1,7 @@
 class Node:
     '''
     Represents an individual element in the Linked List
+    Parameters: data, next
     '''
     def __init__(self, data=None, next=None):
         self.data = data 
@@ -31,9 +32,33 @@ class LinkedList:
     def insert_at_start(self, data):
         '''
         Inserts a new Node at the start of the Linked List
+        1. Creates a new node with the data
+        2. Assignes the current head as the next element in the Linked List
+        3. Sets the new node as the head of the Linked List
+        
+        Time Complexity: O(1)
         '''
         node = Node(data, self.head)
         self.head = node
+
+    def insert_at_end(self, data):
+        '''
+        Inserts a new Node at the end of the Linked List
+        1. Iterates through the entire Linked List
+        2. Point the next pointer of the last element to a new Node which points to None
+
+        Time Complexity: O(n)
+        '''
+        if self.head is None: # If the Linked List is empty
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
+
+
 
 if __name__ == '__main__':
     #####################################################################################################
@@ -49,6 +74,10 @@ if __name__ == '__main__':
     print('\nAdd 1 to the start of the Linked List')
     ll1.insert_at_start(1)
     assert ll1.print_linked_list() == '1-->2-->None', 'either print_linked_list or insert_at_start fails for Linked List with two Nodes'
+
+    print('\nInsert 3 at the end of the Linked List')
+    ll1.insert_at_end(3)
+    assert ll1.print_linked_list() == '1-->2-->3-->None', 'either print_linked_list or insert_at_start fails for Linked List with three Nodes'
 
     print('\nAll tests pass :)' + '\n')
     #####################################################################################################
